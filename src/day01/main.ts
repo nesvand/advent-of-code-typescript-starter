89dd843e0a43d8a14959e922c87f31473f0b8302
Andrew Nesvadba <andrew.nesvadba@neds.com>
2022-12-01 00:09:37 +1000

    chore: add prettier

--- src/day01/main.ts
+++ src/day01/main.ts
@@ -3,17 +3,16 @@
 import { readFile } from 'fs/promises';
 import { part1, part2 } from '.';
 
-(async function() {
+(async function () {
   try {
     const input: string = await readFile('src/day01/resources/input.txt', 'utf8');
 
-    console.log("--- Part One ---");
-    console.log("Result", part1(input));
+    console.log('--- Part One ---');
+    console.log('Result', part1(input));
 
-    console.log("--- Part Two ---");
-    console.log("Result", part2(input));
-
+    console.log('--- Part Two ---');
+    console.log('Result', part2(input));
   } catch (err) {
-      console.error(err);
+    console.error(err);
   }
-})()
+})();
