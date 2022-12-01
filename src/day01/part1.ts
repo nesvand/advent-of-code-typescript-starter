40af7d6883c914bf7268116a2723365aaaa1b79d
Andrew Nesvadba <andrew.nesvadba@neds.com>
2022-12-02 00:51:30 +1000

    chore: styling

--- src/day01/part1.ts
+++ src/day01/part1.ts
@@ -3,7 +3,10 @@
 export function sortedElfCalories(input: string): number[] {
   return input
     .split('\n\n')
-    .reduce((elfCalories, elf) => (elfCalories.push(elf.split('\n').reduce((sum, calories) => sum + parseInt(calories, 10), 0)), elfCalories), [] as number[])
+    .reduce(
+      (elfCalories, elf) => (elfCalories.push(elf.split('\n').reduce((sum, calories) => sum + parseInt(calories, 10), 0)), elfCalories),
+      [] as number[]
+    )
     .sort((a, b) => b - a);
 }
 
