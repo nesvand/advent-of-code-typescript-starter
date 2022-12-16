d884472ce7a11d9142bd17719ee0237a94234ef7
Andrew Nesvadba <andrew.nesvadba@neds.com>
2022-12-16 15:44:27 +1000

    woops

--- test/day15.test.ts
+++ test/day15.test.ts
@@ -41,7 +41,26 @@ if (input) {
 }
 
 test('part two test', () => {
-  expect(part2(``)).toBe(0);
+  expect(
+    part2(
+      `Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+`,
+      10
+    )
+  ).toBe(0);
 });
 
 // if (input) {
